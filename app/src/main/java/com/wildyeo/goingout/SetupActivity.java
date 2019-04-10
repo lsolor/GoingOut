384701b2e35bda388e67b66c73f721d1d56cf299
wildyeo58 <lsolor94@gmail.com>
2019-04-09 22:28:23 -0500

    create set up form

--- app/src/main/java/com/wildyeo/goingout/SetupActivity.java
+++ app/src/main/java/com/wildyeo/goingout/SetupActivity.java
@@ -3,13 +3,11 @@ package com.wildyeo.goingout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
-public class RegisterActivity extends AppCompatActivity {
+public class SetupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-
+        setContentView(R.layout.activity_setup);
     }
 }
--- app/src/main/java/com/wildyeo/goingout/ui/register/RegisterViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.wildyeo.goingout.ui.register;
-
-import android.arch.lifecycle.ViewModel;
-
-public class RegisterViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
