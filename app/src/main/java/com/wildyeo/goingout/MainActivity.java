f4fd795fffb2c35513b37c16a05946509756a756
wildyeo58 <lsolor94@gmail.com>
2019-04-11 20:03:03 -0500

    added navigation item listener

--- app/src/main/java/com/wildyeo/goingout/MainActivity.java
+++ app/src/main/java/com/wildyeo/goingout/MainActivity.java
@@ -1,8 +1,14 @@
 package com.wildyeo.goingout;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -10,12 +16,60 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView recyclerView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //mAuth = FirebaseAuth.getInstance();
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                UserMenuSelector(menuItem);
+                return false;
+            }
+        });
+
+
+    }
+
+    private void UserMenuSelector(MenuItem menuItem) {
+        switch(menuItem.getItemId()){
+            case R.id.nav_profile:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_home:
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_friends:
+                Toast.makeText(this, "Friends", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_find_friends:
+                Toast.makeText(this, "Find Friends", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_go_out:
+                Toast.makeText(this, "Go Out", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_outing:
+                Toast.makeText(this, "Outing", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
 
