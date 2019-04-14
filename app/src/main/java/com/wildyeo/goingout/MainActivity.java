09994bd825a85ee56df123bac50fc38764f08add
wildyeo58 <lsolor94@gmail.com>
2019-04-14 11:56:20 -0500

    refactor layout

--- app/src/main/java/com/wildyeo/goingout/MainActivity.java
+++ app/src/main/java/com/wildyeo/goingout/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
     private RecyclerView postList;
+    private Toolbar mToolbar;
 
 
     @Override
@@ -28,6 +30,12 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Setting up toolbar
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Home");
 
         //mAuth = FirebaseAuth.getInstance();
         drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
