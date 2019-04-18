77d779f245e738947e9c9f9b6eb787463f791b9b
wildyeo58 <lsolor94@gmail.com>
2019-04-18 18:59:34 -0500

    able to send user to register activity from login

--- app/src/main/java/com/wildyeo/goingout/LoginActivity.java
+++ app/src/main/java/com/wildyeo/goingout/LoginActivity.java
@@ -1,13 +1,43 @@
 package com.wildyeo.goingout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button LoginButton;
+    private Button CreateAccountButton;
+    private EditText UserEmail, UserPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        CreateAccountButton = (Button) findViewById(R.id.login_create_account_button);
+        UserEmail = (EditText) findViewById(R.id.login_email);
+        UserPassword = (EditText) findViewById(R.id.login_password);
+        LoginButton = (Button) findViewById(R.id.login_button);
+
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                SendUserToRegisterActivity();
+            }
+        });
+
+    }
+
+    private void SendUserToRegisterActivity()
+    {
+        Intent registerIntent = new Intent(LoginActivity.this,RegisterActivity.class);
+        startActivity(registerIntent);
+        finish();
     }
 }
