eded8c4da6b237c54178a9f53d94fd1e24e7b4ac
wildyeo58 <lsolor94@gmail.com>
2019-05-03 18:34:08 -0500

    need to figure out why navigation view is not importing

--- app/src/main/java/com/wildyeo/goingout/MainActivity.java
+++ app/src/main/java/com/wildyeo/goingout/MainActivity.java
@@ -123,7 +123,8 @@ public class MainActivity extends AppCompatActivity
                 break;
             case R.id.nav_logout:
                 Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
-                break;
+                mAuth.signOut();
+                SendUserToLoginActivity();
         }
     }
 
